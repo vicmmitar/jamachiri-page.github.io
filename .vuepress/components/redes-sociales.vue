b73ceb7bbd3e339f9a2ccf584b804a7c9e8c9f07
vicmmitar <vicmmitar@gmail.com>
2021-12-02 10:17:48 -0400

    redes sociales

--- .vuepress/components/redes-sociales.vue
+++ .vuepress/components/redes-sociales.vue
@@ -5,13 +5,13 @@
   <div class="container mt-4 pt-0">
     <div class="row justify-content-center my-3">
       <div class="botones col-sm-6">
-        <a role="button" class="btn btn-primary" style="background-color: #3b5998;" href="http://facebok.com" target="_blank">
+        <a role="button" class="btn btn-primary" style="background-color: #3b5998;" href="https://www.facebook.com/Jamachiri-100337829170761" target="_blank">
           <i class="bi bi-facebook" style="font-size:24px;"></i>
         </a>
-        <a role="button" class="btn btn-primary" style="background-color: #ac2bac;" href="http://facebok.com" target="_blank">
-          <i class="bi bi-instagram" style="font-size:24px;"></i>
+        <a role="button" class="btn btn-primary" style="background-color: #ed302f;" href="https://www.youtube.com/channel/UCkP4JlPRfjyY8HSTJpTG4yQ" target="_blank">
+          <i class="bi bi-youtube" style="font-size:24px;"></i>
         </a>
-        <a role="button" class="btn btn-primary" style="background-color: #25d366;" href="http://facebok.com" target="_blank">
+        <a role="button" class="btn btn-primary" style="background-color: #25d366;" href="https://wa.me/59167315221" target="_blank">
           <i class="bi bi-whatsapp" style="font-size:24px;"></i>
         </a>
       </div>
